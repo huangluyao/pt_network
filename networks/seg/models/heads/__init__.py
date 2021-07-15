009bbcdcf5f23f5136c270f6d0642e222afa74c1
huangluyao <997911043@qq.com>
2021-07-15 10:13:31 +0800

    add dal mode

--- networks/seg/models/heads/__init__.py
+++ networks/seg/models/heads/__init__.py
@@ -3,5 +3,5 @@ from .deeplabv3_head import DeepLabV3Head
 from .deeplabv3_plus_head import DeepLabV3PlusHead
 from .fcn_head import FCNHead
 from .unet_head import UNetHead
-
+from .dla_head import DLAHead
 __all__ = [k for k in globals().keys() if not k.startswith("_")]
