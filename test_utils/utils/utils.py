6a2490d7319dba27ca42656b1b29870cda786570
huangluyao <997911043@qq.com>
2022-02-16 17:47:43 +0800

    update modify num_classes

--- test_utils/utils/utils.py
+++ test_utils/utils/utils.py
@@ -10,6 +10,16 @@ from networks.seg import build_segmentor
 from networks.gan import build_gan_model
 from test_utils.utils import init_distributed_mode, dist
 
+
+def update_num_classes(num_classes_cfgs, num_classes):
+
+    for c in num_classes_cfgs:
+        if isinstance(c, dict):
+            c.update(dict(num_classes=num_classes))
+        elif isinstance(c, list):
+            update_num_classes(c, num_classes)
+
+
 def build_model(cfg, num_classes, logger):
     model_cfg = cfg.get('model')
     number_classes_model ={"classification":["backbone"],
@@ -17,10 +27,9 @@ def build_model(cfg, num_classes, logger):
                            "segmentation":["decode_head", "auxiliary_head"]
                            }
     num_classes_cfgs = [model_cfg.get(head_cfg, None) for head_cfg in number_classes_model.get(cfg.get("task"), [])]
-    if num_classes_cfgs:
-        for c in num_classes_cfgs:
-            if c is not None:
-                c.update(dict(num_classes=num_classes))
+
+    update_num_classes(num_classes_cfgs, num_classes)
+
     if cfg.get('task') == "classification":
         model = build_classifier(model_cfg)
     elif cfg.get('task') == "detection":
