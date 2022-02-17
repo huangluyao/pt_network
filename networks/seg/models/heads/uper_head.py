364775a9f289d52e7c366351669f3e1cb5403e88
huangluyao <997911043@qq.com>
2022-02-17 10:26:41 +0800

    supoort onnx for upernet

--- networks/seg/models/heads/uper_head.py
+++ networks/seg/models/heads/uper_head.py
@@ -1,9 +1,10 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import numpy as np
 from base.cnn import (ConvModule, resize, )
 from .decode_head import BaseDecodeHead
-
+from test_utils.utils.onnx_symbolic import AdaptiveAvgPool2d
 from ..builder import HEADS
 
 
@@ -20,39 +21,59 @@ class PPM(nn.ModuleList):
         act_cfg (dict): Config of activation layers.
         align_corners (bool): align_corners argument of F.interpolate.
     """
-    def __init__(self, pool_scales, in_channels, channels,
-                                  conv_cfg, norm_cfg, act_cfg, align_corners,
-                                  **kwargs):
-        super(PPM, self).__init__()
 
+    def __init__(self, pool_scales, in_channels, channels, conv_cfg, norm_cfg,
+                 act_cfg, align_corners, **kwargs):
+        super(PPM, self).__init__()
         self.pool_scales = pool_scales
         self.align_corners = align_corners
         self.in_channels = in_channels
         self.channels = channels
-        for pool_scale in pool_scales:
-            self.append(nn.Sequential(
-                nn.AdaptiveAvgPool2d(pool_scale),
-                ConvModule(self.in_channels,
-                           self.channels,
-                           1,
-                           conv_cfg=conv_cfg,
-                           norm_cfg=norm_cfg,
-                           act_cfg=act_cfg,
-                           **kwargs
-                           )
-            ))
+        self.conv_cfg = conv_cfg
+        self.norm_cfg = norm_cfg
+        self.act_cfg = act_cfg
+        self.ppm_blocks = nn.ModuleList()
+        for _ in pool_scales:
+            self.ppm_blocks.append(
+                ConvModule(
+                    self.in_channels,
+                    self.channels,
+                    1,
+                    conv_cfg=self.conv_cfg,
+                    norm_cfg=self.norm_cfg,
+                    act_cfg=self.act_cfg,
+
+                    **kwargs))
 
     def forward(self, x):
+        _, _, h, w = x.shape
+        """Forward function."""
         ppm_outs = []
-        for ppm in self:
-            ppm_out = ppm(x)
-            upsampled_ppm_out = resize(ppm_out,
-                                       size=x.size()[2:],
-                                       mode='bilinear',
-                                       align_corners=self.align_corners
-                                       )
+        for idx in range(len(self.pool_scales)):
+            if self.pool_scales[idx] == 1:
+                kernel_0, kernel_1, stride_0, stride_1 = int(h), int(w), int(h), int(w)
+            else:
+                # eg 1: this method could not trans to onnx, is same to original adaptive avgpool2d()
+                kernel_0 = int(np.floor((h + self.pool_scales[idx] - 1) / self.pool_scales[idx]))
+                kernel_1 = int(np.floor((w + self.pool_scales[idx] - 1) / self.pool_scales[idx]))
+                stride_0 = int(np.floor((h - kernel_0) / (self.pool_scales[idx] - 1)))
+                stride_1 = int(np.floor((w - kernel_1) / (self.pool_scales[idx] - 1)))
+                # eg 2:
+                # stride_0 = int(np.floor(h / self.pool_scales[idx]))
+                # stride_1 = int(np.floor(w / self.pool_scales[idx]))
+                # kernel_0 = int(h - (self.pool_scales[idx] - 1) * stride_0)
+                # kernel_1 = int(w - (self.pool_scales[idx] - 1) * stride_1)
+            if 0 in [stride_0, stride_1, kernel_1, kernel_0]:
+                x_avg = x
+            else:
+                x_avg = F.avg_pool2d(x, (kernel_0, kernel_1), stride=(stride_0, stride_1))
+            ppm_out = self.ppm_blocks[idx](x_avg)
+            upsampled_ppm_out = resize(
+                ppm_out,
+                size=x.size()[2:],
+                mode='bilinear',
+                align_corners=self.align_corners)
             ppm_outs.append(upsampled_ppm_out)
-
         return ppm_outs
 
 
